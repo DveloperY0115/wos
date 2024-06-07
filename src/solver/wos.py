b393530ce19b6ce68299be7e17cbc2ea3a6572b3
Seungwoo Yoo <dreamy1534@gmail.com>
2024-06-07 15:02:17 +0900

    Implement function 'wos'

--- src/solver/wos.py
+++ src/solver/wos.py
@@ -4,26 +4,49 @@ wos.py
 A Taichi implementation of the Walk-on-Spheres algorithm.
 """
 
-from typing import Any
-
 import taichi as ti
+import taichi.math as tm
 
+from src.utils.sampling import (
+    uniform_ball,
+)
 from src.utils.types import (
+    Float1DArray,
     Scene,
     VectorField,
 )
 
 
 @ti.func
-def wos_step(
-    query_pts: VectorField,
+def wos_walk(
+    query_pt: tm.vec3,
     scene: Scene,
+    n_step: int,
     eps: float,
 ):
     """
     Takes a single step of the Walk-on-Spheres algorithm.
     """
-    pass
+    curr_pt = query_pt
+
+    for _ in range(n_step):
+
+        # Compute the distance to the closest boundary point
+        dist = scene.query_dist(curr_pt)
+
+        # If distance < eps, terminate the walk
+        if ti.abs(dist) < eps:
+            break
+
+        # Sample a random point on the sphere whose
+        # radius is the distance to the closest boundary point
+        curr_pt = uniform_ball(ti.abs(dist), curr_pt)
+
+    # Retrieve the boundary value
+    bd_val = scene.query_boundary(curr_pt)
+
+    return bd_val
+
 
 @ti.kernel
 def wos(
@@ -31,10 +54,20 @@ def wos(
     scene: Scene,
     eps: float,
     n_walk: int,
+    n_step: int,
+    sol_: Float1DArray,
 ):
     """
     Simulates the Walk-on-Spheres algorithm.
+
+    Args:
+    - n_step: The number of maximum steps for each random walk
     """
     for i in range(query_pts.shape[0]):  # Parallelized
         for _ in range(n_walk):  # Sequential
-            wos_step(query_pts, scene, eps)
+            bd_val = wos_walk(
+                query_pts[i], scene, n_step, eps
+
+            )
+            sol_[i] += bd_val
+        sol_[i] /= n_walk
