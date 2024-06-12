7ea689b3b0863abdbca2d2611724a817eee43d4f
Seungwoo Yoo <dreamy1534@gmail.com>
2024-06-13 00:02:20 +0900

    Remove lines

--- scripts/misc/normalize_mesh.py
+++ scripts/misc/normalize_mesh.py
@@ -26,12 +26,8 @@ def main(args: Args):
     # Load mesh
     v, f = igl.read_triangle_mesh(str(args.mesh_file))
     
-    # print(v.max(axis=0), v.min(axis=0))
-    # return
-
     # Normalize mesh
     v = normalize_mesh(v, method=args.method)    
-    print(v.max(axis=0), v.min(axis=0))
 
     # Save normalized mesh
     igl.write_triangle_mesh(str(out_file), v, f)
