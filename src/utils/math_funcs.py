33293ef1eb47fcf65b4a29aebb5ef829c3632977
Seungwoo Yoo <dreamy1534@gmail.com>
2024-08-30 00:23:22 +0900

    Resolve numerical issue in Green's function

--- src/utils/math_funcs.py
+++ src/utils/math_funcs.py
@@ -9,6 +9,13 @@ import taichi.math as tm
 from .constants import EPS
 
 @ti.func
+def safe_length(v: tm.vec3, eps: float = EPS):
+    """
+    Computes the length of a vector with a small epsilon.
+    """
+    return tm.max(tm.length(v), eps)
+
+@ti.func
 def sinh(x: float):
     """
     Computes the hyperbolic sine of the input value.
@@ -27,7 +34,7 @@ def volume_ball(radius: float):
     """
     Computes the volume of a ball in 3D space.
     """
-    return (4 / 3) * np.pi * radius ** 3
+    return (4 / 3) * np.pi * (radius ** 3)
 
 @ti.func
 def harmonic_green_2d(x: tm.vec2, y: tm.vec2, R: float):
@@ -47,7 +54,12 @@ def harmonic_green_2d(x: tm.vec2, y: tm.vec2, R: float):
     return val
 
 @ti.func
-def harmonic_green_3d(x: tm.vec3, y: tm.vec3, R: float):
+def harmonic_green_3d(
+    x: tm.vec3,
+    y: tm.vec3,
+    R: float,
+    r_clamp: float = 1e-4,
+):
     """
     Computes the harmonic Green's function defined in 3D space.
 
@@ -57,10 +69,11 @@ def harmonic_green_3d(x: tm.vec3, y: tm.vec3, R: float):
     The function is defined as follows [Sawhney and Crane, 2020]:
     G(x, y) = (1 / 4 * pi) * ((R - || x - y ||) / || x - y || * R)
     """
-    R_ = R + EPS
-    val = (1 / (4 * np.pi)) * ((R_ - tm.length(x - y)) / ((tm.length(x - y) * R_) + EPS))
-    if tm.isnan(val) or tm.isinf(val):  # TODO: Why NaNs..?
-        val = 0.0
+    r = safe_length(y - x, r_clamp)
+    if r >= R:
+        r = R / 2.0
+    assert r < R, f"'r' must be less than 'R'. Got {r} and {R}."
+    val = (1 / (4 * np.pi)) * (1 / r - 1 / R)
     return val
 
 @ti.func
